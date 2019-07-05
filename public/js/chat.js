097d1774dd965dd4ec32779e7edcdcab04055d23
Daniel Galván <danygs@gmail.com>
2019-07-05 17:52:20 -0500

    Added timestamps to messages and browser visibility

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,64 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.querySelector('#messageForm')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationTemplate = document.querySelector('#location-template').innerHTML
+
 socket.on('message', (message) => {
   console.log(message)
+  const html = Mustache.render(messageTemplate, {
+    message: message.text,
+    createdAt: moment(message.createdAt).format('h:mm a')
+  })
+  $messages.insertAdjacentHTML('beforeend', html)
 })
 
-document.querySelector('#messageForm').addEventListener('submit', (e) => {
+socket.on('locationMessage', (url) => {
+  console.log(url)
+  const html = Mustache.render(locationTemplate, {
+    url
+  })
+  $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (e) => {
   e.preventDefault()
+
+  $messageFormButton.setAttribute('disabled', 'disabled')
+
   const input = e.target.elements.message.value
-  socket.emit('sendMessage', input)
+  socket.emit('sendMessage', input, (error) => {
+    $messageFormButton.removeAttribute('disabled')
+    $messageFormInput.value = ''
+    $messageFormInput.focus()
+
+    if(error) {
+      return console.log(error)
+    }
+    console.log('The message was delivered')
+  })
+})
+
+$sendLocationButton.addEventListener('click', () => {
+  $sendLocationButton.setAttribute('disabled', 'disabled')
+  if(!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser')
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', { 
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    }, () => {
+      console.log('Location shared!')
+      $sendLocationButton.removeAttribute('disabled')
+    })
+  })
 })
--- src/app.js
+++ src/app.js
@@ -1,23 +1,16 @@
 const path = require('path')
 const express = require('express')
-const hbs = require('hbs')
 const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
-app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
-
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-  res.render('index', {
-    title: 'Chat Application',
-    name: 'Daniel Galván'
-  })
+  res.render('index')
 })
 
 module.exports = app
