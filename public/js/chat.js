f5e2286dc3f0f9076ec03113e9dabe282d6655b6
Daniel Galván <daniel@triyond.com>
2019-07-07 21:41:31 -0500

    User Interface added and tracking users

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,22 +10,61 @@ const $messages = document.querySelector('#messages')
 //Templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+//Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true})
+
+const autoScroll = () => {
+  // New message element
+  const $newMessage = $messages.lastElementChild
+  // Height of the new message
+  const newMessageStyles = getComputedStyle($newMessage)
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+  // Visible Height
+  const visibleHeight = $messages.offsetHeight
+  // Height of messages container
+
+  const containerHeight = $messages.scrollHeight
+
+  // How far have I scrolled
+  const scrollOffset = $messages.scrollTop + visibleHeight
+
+  if (containerHeight - newMessageHeight <= scrollOffset) {
+    $messages.scrollTop = $messages.scrollHeight
+  }
+}
 
 socket.on('message', (message) => {
   console.log(message)
   const html = Mustache.render(messageTemplate, {
+    username: message.username,
     message: message.text,
     createdAt: moment(message.createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend', html)
+  autoScroll()
 })
 
-socket.on('locationMessage', (url) => {
-  console.log(url)
+socket.on('locationMessage', (message) => {
+  console.log(message)
   const html = Mustache.render(locationTemplate, {
-    url
+    username: message.username,
+    url: message.url,
+    createdAt: moment(message.createdAt).format('h:mm a')
   })
   $messages.insertAdjacentHTML('beforeend', html)
+  autoScroll()
+})
+
+socket.on('roomData', ({ room, users}) => {
+  const html = Mustache.render(sidebarTemplate, {
+    room,
+    users
+  })
+  document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit', (e) => {
@@ -61,4 +100,11 @@ $sendLocationButton.addEventListener('click', () => {
       $sendLocationButton.removeAttribute('disabled')
     })
   })
+})
+
+socket.emit('join', { username, room }, (error) => {
+  if(error) {
+    alert(error)
+    location.href = '/'
+  }
 })
